0f7f32fddbf4781856b993672304020695a141ca
Diego <d.garciabrisa@gmail.com>
2021-04-07 19:26:52 +0200

    app set up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+      
+      <Switch>
+
+        <Route />
+
+      </Switch>
+      
+      </BrowserRouter>
       
     </div>
   );
