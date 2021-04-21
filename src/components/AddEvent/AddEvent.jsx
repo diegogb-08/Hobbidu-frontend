6a5d9b0a6b1e2c6fae4da7f0f1c7e7e56185d9d5
diego <d.garciabrisa@gmail.com>
2021-04-21 19:06:12 +0200

    adding friends to create event

--- src/components/AddEvent/AddEvent.jsx
+++ src/components/AddEvent/AddEvent.jsx
@@ -3,6 +3,13 @@ import Modal from '../Modal/Modal';
 import GeoLocation from '../GeoLocation/GeoLocation';
 import { connect } from 'react-redux';
 import Button from '../Button/Button';
+import axios from 'axios';
+import {port,customer, search, query} from '../../tools/apiPaths';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserPlus, faUserTimes } from '@fortawesome/free-solid-svg-icons';
+// import Avatar from '../Avatar/Avatar';
+
+
 
 const AddEvent = (props) => {
     // Modal Hook
@@ -22,19 +29,46 @@ const AddEvent = (props) => {
         maxJoiners: 0,
         description: '',
         location: props.location,
-
     })
 
-    const handleChange = () => {
+    console.log(event.joiners)
 
+    const [suggestion, setSuggestion] = useState([])
+
+    const [friends, setFriends] = useState([])
+    console.log(friends)
+    
+    //Handlers
+
+    const handleChange = (e) => {
+        setEvent({...event, [e.target.name]: e.target.value, [e.target.name]: e.target.value});
+    }
+
+    const handleJoiners = async (e) => {
+
+        let result = await axios.get(port+customer+search+query+e.target.value)
+        setSuggestion(result.data)
+    }
+
+    const handleSelected = (user) => {
+        let maxJoiners = parseInt(event.maxJoiners)
+        let joinerIdFound = event.joiners.find(id => id === user._id )
+        if (maxJoiners !== event.joiners.length && joinerIdFound === undefined){
+            setEvent({...event, joiners: [...event.joiners, user._id]});
+            setFriends([...friends, user])
+        }
     }
 
     // Functions
 
     const selectHobby = (hobby_id) => {
-
         setSelected({[hobby_id]:{backgroundColor: '#f05356', color: 'white'}})
-        console.log(hobby_id)
+        setEvent({...event, hobby_id : hobby_id})
+    }
+
+    const deleteFriend = (user) => {
+        setEvent(event.joiners.filter(element => element !== user._id));
+        setFriends(friends.filter(element => element._id !== user._id))
     }
 
     return (
@@ -74,8 +108,51 @@ const AddEvent = (props) => {
                             </div>
                             <div className="addEventSections">
                                 <div className="inputEvent">
+                                    <p className="titles">Max. joiners</p>
+                                    <select className="selector" name="maxJoiners" onChange={handleChange} >
+                                        <option value={0} defaultValue>0</option>
+                                        <option value={1} >1</option>
+                                        <option value={2}>2</option>
+                                        <option value={3}>3</option>
+                                        <option value={4}>4</option>
+                                        <option value={5}>5</option>
+                                        <option value={6}>6</option>
+                                        <option value={7}>7</option>
+                                        <option value={8}>8</option>
+                                        <option value={9}>9</option>
+                                        <option value={10}>10</option>
+                                    </select>
+
+                                </div>
+                                <div className="inputEvent">
+                                    <div className="grid">
+                                        {
+                                            friends?.map(friend => {
+                                                return (
+                                                    <div className="friend" key={friend._id}>
+                                                        <FontAwesomeIcon icon={faUserTimes} className="iconFriend" onClick={()=>deleteFriend(friend)} />
+                                                        <p>{friend.name}</p>
+                                                    </div>
+                                                )
+                                            })
+                                        }
+                                    </div>
                                     <p className="titles">Add a friend</p>
-                                    <input type="text" name="joiner" maxLength="20" onChange={handleChange} />
+                                    <input type="text" name="joiners" maxLength="20" onChange={handleJoiners} placeholder="Introduce a name"/>
+                                    <div className="autocompleteDropdownContainer">
+                                        {
+                                            suggestion?.map(user => {
+                                                return (
+                                                    <div className="suggestion" key={user._id} onClick={()=>handleSelected(user)}>
+                                                        <FontAwesomeIcon icon={faUserPlus} className="iconSuggestion" />
+                                                        {/* <Avatar /> */}
+                                                        <p>{user.name}</p>
+                                                    </div>
+                                                )
+                                            })
+                                        }
+                                    </div>
+                                    
                                 </div>
                                 <div className="inputEvent">
                                     <p className="titles">Description</p>
