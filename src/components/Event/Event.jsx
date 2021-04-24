604844d4b1a77ae081862b6ceb365bd3cc5e09f7
diego <d.garciabrisa@gmail.com>
2021-04-24 20:21:01 +0200

    get event creator info

--- src/components/Event/Event.jsx
+++ src/components/Event/Event.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
+import axios from 'axios'
+import { customer, meeting, port } from '../../tools/apiPaths'
 import ControlPanel from '../ControlPanel/ControlPanel'
 import Footer from '../Footer/Footer'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
+import moment from 'moment'
+import Avatar from '../Avatar/Avatar'
+import { faUserCircle } from '@fortawesome/free-regular-svg-icons'
 
 const Event = (props) => {
+
+    const [event, setEvent] = useState({})
+    const [creator, setCreator] = useState({})
+
+    console.log(event)
+    console.log(creator)
+
+    useEffect(()=>{
+        getEvent()
+        // eslint-disable-next-line
+    },[])
+
+    useEffect(()=>{
+        getUser()
+    },[event])
+
+    // Functions
+
+    const getEvent = async () => {
+
+        try{
+            let result = await axios.get(port+meeting+'/'+props.event._id)
+            if(result.data)
+                setEvent(result.data)
+        }catch(err){
+            
+        }
+    }
+
+    const getUser = async () => {
+
+        try{
+            let result = await axios.get(port+customer+'/'+event.user_id)
+            if(result.data)
+                return setCreator(result.data)
+        }catch(err){
+            
+        }
+    }
+
     return (
         <div className="eventComponent">
             <div className="spacer"></div>
@@ -12,9 +59,37 @@ const Event = (props) => {
             <div className="spacer"></div>
             <ControlPanel/>
             <div className="eventCointainer">
-                    
-            </div>
+                    <h2 className="title">{props.event.title}</h2>
+                    <div className="eventInfoContainer">
+                        <div className="eventInfo">
+                            <div className="eventInfoLeft">
+                                <div className="location">
+                                    <FontAwesomeIcon icon={faMapMarkerAlt} className="icon"/>
+                                    <p>{event.location?.name}</p>
+                                </div>
+                                <h3>{moment(event?.event_date).format('Do MMMM YYYY, h:mm a')}</h3>
+                                <div className="createdBy">
+                                    {
+                                        !creator.profile_img ?
+                                        <div className="iconBtnAvatar">
+                                            <Avatar profilePic={creator.profile_img}/>
+                                        </div>
+                                        :
+                                        <FontAwesomeIcon icon={faUserCircle} className={'iconBtn'}/>
+                                        
+                                    }
+                                    <p>{creator.user_name}</p>
+                                </div>
+                            </div>
+                            <div className="evenInfoRight">
 
+                            </div>
+                        </div>
+                        <div className="comments">
+
+                        </div>
+                    </div>
+            </div>
             <Footer/>
         </div>
     )
@@ -24,7 +99,7 @@ const mapStateToProps = state => {
     return {
         user : state.userReducer.user,
         location: state.userReducer.location,
-        event : state.eventReducer
+        event : state.eventReducer.event
     }
 }
 
